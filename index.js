5cd2e373bfb8c416246930a08916f2056e6d856c
Naimur Rahman <naimur2231@gmail.com>
2023-03-15 22:18:06 +0600

    display quiz questions and answers bug fixed

--- index.js
+++ index.js
@@ -59,10 +59,10 @@ const displayQuiz = (data) => {
   }
 // console.log(data)
 data.forEach((quiz, i) => {
-  console.log(quiz)
-  console.log(quiz.question)
-  console.log(quiz.options)
-  console.log(i)
+  // console.log(quiz)
+  // console.log(quiz.question)
+  // console.log(quiz.options)
+  // console.log(i)
     quizContainer.innerHTML += `<div class="m-3 py-3 px-4 shadow-sm rounded">
   <div class="flex items-center">
     <div class="h-8 w-8 bg-green-300 rounded-full flex justify-center items-center text-green-800 mr-3">
@@ -71,7 +71,7 @@ data.forEach((quiz, i) => {
     <p class="text-gray-800 text-sm">${quiz.question}</p>
   </div>
   <div class="grid grid-cols-2 gap-4 mt-5">
-    ${displayQuizOptions(quiz.options[0], i)}
+    ${displayQuizOptions(quiz.options, i)}
   </div>
 </div>`;
   });
