e84788f7f32bff70f9d8566b610fd627095f85fa
Naimur Rahman <naimur2231@gmail.com>
2023-03-15 21:36:30 +0600

    display quiz data bug fixed

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ startQuiz.addEventListener("click", () => {
 // All quiz data fetched from json
 const loadQuiz = async () => {
   const res = await fetch("./data/quiz.json");
-  const data = await res.json;
+  const data = await res.json();
   quizData = data;
   displayQuiz(data);
 };
@@ -57,17 +57,21 @@ const displayQuiz = (data) => {
     quizContainer.innerHTML = "";
     return;
   }
-
-  data.forEach((quiz, i) => {
+// console.log(data)
+data.forEach((quiz, i) => {
+  console.log(quiz)
+  console.log(quiz.question)
+  console.log(quiz.options)
+  console.log(i)
     quizContainer.innerHTML += `<div class="m-3 py-3 px-4 shadow-sm rounded">
   <div class="flex items-center">
     <div class="h-8 w-8 bg-green-300 rounded-full flex justify-center items-center text-green-800 mr-3">
       ${i + 1}
     </div>
-    <p class="text-gray-800 text-sm">${quiz.quetion}</p>
+    <p class="text-gray-800 text-sm">${quiz.question}</p>
   </div>
   <div class="grid grid-cols-2 gap-4 mt-5">
-    ${displayQuizOptions(quiz.options, i)}
+    ${displayQuizOptions(quiz.options[0], i)}
   </div>
 </div>`;
   });
