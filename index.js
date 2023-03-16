a9b7a34e7257ab88ea4bfdaa111dda13517d086b
Naimur Rahman <naimur2231@gmail.com>
2023-03-16 10:52:22 +0600

    quiz hero all bug fixed

--- index.js
+++ index.js
@@ -58,9 +58,9 @@ const displayQuiz = (data) => {
     return;
   }
 
+  // Displaying quiz questions and answers  
 data.forEach((quiz, i) => {
   // console.log(quiz);
-
     quizContainer.innerHTML += `<div class="m-3 py-3 px-4 shadow-sm rounded">
   <div class="flex items-center">
     <div class="h-8 w-8 bg-green-300 rounded-full flex justify-center items-center text-green-800 mr-3">
